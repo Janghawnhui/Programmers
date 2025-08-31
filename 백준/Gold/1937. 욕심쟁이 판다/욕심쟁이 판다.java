baa6d09938674d40fa84c706ec32da5c2a93acd9
Janghawnhui <68009882+Janghawnhui@users.noreply.github.com>
2025-08-31 23:47:33 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 1088 ms, Memory: 218248 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -5,40 +5,50 @@ public class Main {
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
 
-    static int dfs(int x, int y,int n, int[][]box, int answer) {
-        if(memoization[x][y]!=0) return memoization[x][y];
+    static int dfs(int x, int y,int n, int[][]box) {
+
+        if(memoization[x][y]!= -1) return memoization[x][y];
+
         memoization[x][y]=1;
         for(int i=0; i<4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx <0 || ny<0 || n<=nx || n<=ny) continue;
-            if(box[nx][ny] <= box[x][y]) continue;
 
-            memoization[x][y] = Math.max(memoization[x][y], dfs(nx, ny, n, box, 0) + 1);
+            if(nx <0 || ny<0 || n<=nx || n<=ny) continue;
+            if(box[x][y] >= box[nx][ny]) continue;
+            memoization[x][y] =  Math.max(memoization[x][y], dfs(nx, ny,n ,box)+1);
         }
+
         return memoization[x][y];
     }
     public static void main(String[] args) {
         int answer;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         int[][] box = new int[n][n];
         int real_answer=0;
+
         memoization = new int[n][n];
 
         for(int i=0; i<n;i++) {
             for(int j =0; j<n;j++) {
                 box[i][j] =sc.nextInt();
+                memoization[i][j] =-1;
             }
         }
 
         for(int i= 0; i<n; i++) {
             for(int j= 0; j<n; j++) {
-                answer =dfs(i,j,n, box,0);
+                answer =dfs(i,j,n, box);
+
                 if(real_answer <= answer) {
                     real_answer = answer;
                 }
+
+                //System.out.print(memoization[i][j] + " ");
             }
+            //System.out.println("");
         }
         System.out.println(real_answer);
     }
